cdb79902414e33c6208cb68c408601df26c9d235
Madhan raj S <110301462+Lalithraju19@users.noreply.github.com>
2022-11-15 18:58:23 +0530

    Create initial.py

---
+++ Project Development Phase/Sprint 1/Home/Migration/initial.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('profession', models.CharField(choices=[('Employee', 'Employee'), ('Business', 'Business'), ('Student', 'Student'), ('Other', 'Other')], max_length=10)),
+                ('Savings', models.IntegerField(blank=True, null=True)),
+                ('income', models.BigIntegerField(blank=True, null=True)),
+                ('image', models.ImageField(blank=True, upload_to='profile_image')),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
